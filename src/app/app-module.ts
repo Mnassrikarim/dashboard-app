45198ebfa93192ce5f85c71c92f93f67d0ba315f
Mnassrikarim <kmn242424@gmail.com>
2026-02-18 17:51:46 +0100

    first

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Services } from './pages/services/services';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    Sidebar,
+    Dashboard,
+    Services
   ],
   imports: [
     BrowserModule,
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,16 @@
+import { Services } from './pages/services/services';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'services', component: Services }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
